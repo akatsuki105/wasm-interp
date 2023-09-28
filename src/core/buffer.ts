eeb7e9d0a710e522b846c597bc672527011d5a82
pokemium <bluejapan73+dev@gmail.com>
2023-09-29 08:40:53 +0900

    chap 2.2.1

--- src/core/buffer.ts
+++ src/core/buffer.ts
@@ -11,6 +11,13 @@ export class Buffer {
     this.#buffer = buffer;
   }
 
+  get byteLength(): number {
+    return this.#buffer.byteLength;
+  }
+  get eof(): boolean {
+    return this.byteLength <= this.#cursor;
+  }
+
   readBytes(size: number): Uint8Array {
     if (this.#buffer.byteLength < this.#cursor + size) {
       return new Uint8Array(0);
@@ -18,5 +25,62 @@ export class Buffer {
     const slice = this.#buffer.slice(this.#cursor, this.#cursor + size);
     this.#cursor += size;
     return new Uint8Array(slice);
+  }
+
+  readByte(): number {
+    const bytes = this.readBytes(1);
+    if (bytes.length <= 0) {
+      return -1;
+    }
+    return bytes[0];
+  }
+
+  // LEB128
+  readU32(): number {
+    let result = 0;
+    let shift = 0;
+    while (true) {
+      const byte = this.readByte();
+      result |= (byte & 0b01111111) << shift;
+      shift += 7;
+      if ((0b10000000 & byte) === 0) {
+        return result;
+      }
+    }
+  }
+
+  readS32(): number {
+    // https://en.wikipedia.org/wiki/LEB128#Decode_signed_32-bit_integer
+    let result = 0;
+    let shift = 0;
+    while (true) {
+      const byte = this.readByte();
+      result |= (byte & 0b01111111) << shift;
+      shift += 7;
+      if ((0b10000000 & byte) === 0) {
+        if (shift < 32 && (byte & 0b01000000) !== 0) {
+          return result | (~0 << shift);
+        }
+        return result;
+      }
+    }
+  }
+
+  readI32(): number {
+    return this.readS32();
+  }
+
+  readBuffer(size: number = this.#buffer.byteLength - this.#cursor): Buffer {
+    return new Buffer(this.readBytes(size));
+  }
+
+  // [要素数, 要素1, 要素2, ...]
+  readVec<T>(readT: () => T): T[] {
+    const vec = [];
+    const size = this.readU32();
+    for (let i = 0; i < size; i++) {
+      vec.push(readT());
+    }
+    return vec;
   }
 }
